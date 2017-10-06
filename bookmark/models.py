3e5183543db49e1da5405dfc26b8627cdf60b483
Snowfinger <Snowfinger@SMCo-Mac-Pro.local>
2017-10-07 00:34:15 +0900

    final

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Bookmark(models.Model):
+    author = models.ForeignKey(User, null=True)
     title = models.CharField(max_length=100, blank=True, null=True)
     url = models.URLField('url', unique=True)
 
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
-from django.views.generic import DetailView, ListView, DeleteView
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView, ListView, DeleteView, CreateView, UpdateView
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Bookmark
 
-class BookmarkListV(ListView):
+class BookmarkListV(LoginRequiredMixin, ListView):
     model = Bookmark
 
-class BookmarkDetailV(DetailView):
+class BookmarkDetailV(LoginRequiredMixin, DetailView):
     model = Bookmark
+
+class BookmarkCreateV(LoginRequiredMixin, CreateView):
+    model = Bookmark
+    fields = ['title', 'url']
+    success_url = reverse_lazy('bookmark:index')
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid:
+            form.instance.save()
+            return redirect('/bookmark/')
+        else:
+            return self.render_to_response({'form':form})
+
+class BookmarkUpdateV(LoginRequiredMixin, UpdateView):
+    model = Bookmark
+    fields = ['title', 'url']
+    success_url = reverse_lazy('bookmark:index')
+
+class BookmarkDeleteV(LoginRequiredMixin, DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('bookmark:index')
